4e3f9eeb790e75b89c108abc60a532309d383185
b.shubham <b.shubham@aitglobalinc.com>
2020-09-20 13:23:20 +0530

    First commit

---
+++ src/com/codeyogi/servlet/AddServlet.java
@@ -0,0 +1,28 @@
+package com.codeyogi.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AddServlet
+ */
+//@WebServlet("/add")
+public class AddServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int k = Integer.parseInt(request.getParameter("num1")) + Integer.parseInt(request.getParameter("num2"));
+
+		response.getWriter().print("Addition is: " + k);
+	}
+
+}
